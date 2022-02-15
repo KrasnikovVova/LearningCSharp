b21dea35c65fc8d532de8827db4cad67973f9a3e
222 <222@DESKTOP-5N4LB11>
2022-02-16 00:10:44 +0300

    Добавьте файлы проекта.

---
+++ Красников_задания_асmр.ru/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Красников_задания_асmр.ru
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
